f08fe2a358dbb7d322b2f761d798abb10cc2798d
shelly <baruch.shelly@gmail.com>
2023-12-17 15:23:33 +0200

    version before useContext

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {Header} from "./components/header";
+import {Footer} from "./components/footer";
+import {useEffect, useState} from "react";
+import {Outlet} from "react-router-dom";
+
+
+
+export function App() {
+    const [postsArray, setPostsArray] = useState([]);
+    const addPost = (newPost) =>
+        setPostsArray([...postsArray, newPost])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => setPostsArray(json))
+    }, []);
+
+    return (
+    <div className="page">
+        <Header/>
+        <Outlet context={[postsArray]} />
+        <Footer />
     </div>
   );
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Home} from "./pages/home";
+import {Posts} from "./pages/posts";
+import {Post} from "./pages/post";
+import {About} from "./pages/about";
+import {Contact} from "./pages/contact";
+// import {Admin} from "./pages/admin";
+import {App} from './App';
+
+
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+            {
+                path: "/posts",
+                element: <Posts />,
+            },
+            {
+                path: "/posts/:id",
+                element: <Post />,
+            },
+            {
+                path: "/Contact",
+                element: <Contact />,
+            },
+
+        ],
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
